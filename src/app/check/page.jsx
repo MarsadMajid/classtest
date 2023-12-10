27d05b84de9a0aef3739c0409054576a888841cc
Marsad <marsadmajid0@gmail.com>
2023-12-11 00:38:19 +0500

    hello

--- src/app/check/page.jsx
+++ src/app/check/page.jsx
@@ -1,22 +1,22 @@
 import React from 'react'
 
 
-const fetchBills = async ()=>{
-    try {
-      const bills = await prisma.Bills.findMany()
-      console.log(bills)
-      return bills
-    } catch (error) {
-      console.log("error",error);
-    }
-  }
+// const fetchBills = async ()=>{
+//     try {
+//       const bills = await prisma.Bills.findMany()
+//       console.log(bills)
+//       return bills
+//     } catch (error) {
+//       console.log("error",error);
+//     }
+//   }
 const page =  async () => {
     const bills = await fetchBills()
   return (
     <div>
   <div >
       <h2>Bill List</h2>
-      <table>
+      {/* <table>
         <thead>
           <tr >
             <th>Consumer</th>
@@ -24,18 +24,18 @@ const page =  async () => {
             <th>Address</th>
             <th >Status</th>
           </tr>
-        </thead>
-        <tbody>
+        </thead> */}
+        {/* <tbody>
           {bills?.map((bill) => (
             <tr key={bill.id} >
               <td>{bill.consumerNumber}</td>
               <td>{bill.units}</td>
               <td>{bill.address}</td>
               {/* <td className={bill.status ? paid : notpaid}>{bill.status ? "Paid" : "Not Paid"}</td> */}
-            </tr>
-          ))}
-        </tbody>
-      </table>
+            {/* </tr> */}
+          {/* ))} */}
+        {/* </tbody> */}
+      {/* </table> */} */
     </div>
     </div>
   )
